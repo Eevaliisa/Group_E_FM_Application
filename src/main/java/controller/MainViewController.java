c4111c261b326c2afb81d9186d8ccc1dc96d16b3
Ilya Garkusha <ilja.garkusha94@gmail.com>
2020-04-21 18:53:52 +0300

    -"contact Admin feature" resolved

--- src/main/java/controller/MainViewController.java
+++ src/main/java/controller/MainViewController.java
@@ -1,7 +1,9 @@
 package controller;
 
+
 import dao.JobDAOImpl;
 import entity.Job;
+
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,13 +14,20 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import main.Main;
 
+
+
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.*;
+
 
 public class MainViewController implements Initializable {
 
@@ -248,7 +257,6 @@ public class MainViewController implements Initializable {
 
         tableviewUpdateJob.setItems(jobObservableList);
 
-
         tableUpdateColumnEquipment.setCellFactory(TextFieldTableCell.forTableColumn());
         tableUpdateColumnEquipment.setOnEditCommit(event -> {
             event.getTableView().getItems().get(event.getTablePosition().getRow()).setEquipmentName(event.getNewValue());
@@ -416,6 +424,71 @@ public class MainViewController implements Initializable {
         fillInProgressTable();
         fillTable();
         fillUpdateTable();
+    }
+
+    @FXML
+    void sendMessage() {
+        // Recipient's email ID needs to be mentioned.
+        String to = "javatallinn@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "javatallinn@gmail.com";
+        final String username = "javatallinn@gmail.com";//change accordingly
+        final String password = "pysruh-wowsej-kIgre8";//change accordingly
+
+        // Assuming you are sending email through relay.jangosmtp.net
+        String host = "smtp.gmail.com";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+
+        // Get the Session object.
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+
+            // Set Subject: header field
+            message.setSubject(textFieldContactAdminSubject.getText());
+
+            // Now set the actual message
+            message.setText("Customer email: " + textFieldContactAdminID.getText()
+                    +"\nCustomer name: " + textFieldContactAdminName.getText() + "\n" + "\n"
+                    +textFieldContactAdminDescription.getText());
+
+            // Send message
+            Transport.send(message);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Sent message successfully");
+            alert.setTitle("Success!");
+            alert.show();
+
+            textFieldContactAdminName.clear();
+            textFieldContactAdminID.clear();
+            textFieldContactAdminDescription.clear();
+            textFieldContactAdminSubject.clear();
+
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
